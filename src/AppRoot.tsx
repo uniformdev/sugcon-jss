b3e7f7bfed673049b83f86eb984f135401143feb
Alex Shyba <as@altola.co>
2020-04-29 16:31:40 -0700

    js optimizaiton 3: removing apollo and font-awesome

--- src/AppRoot.tsx
+++ src/AppRoot.tsx
@@ -5,26 +5,10 @@ import { RouteDataManager } from "./lib/RouteDataManager";
 import { useAppStateContext } from "./lib/AppStateProvider";
 import { ComponentFactoryContextProvider } from "./lib/ComponentFactoryProvider";
 import { SitecoreContextReactContext as ComponentsSitecoreContext } from "./lib/SitecoreContextReactContext";
-import { ApolloProvider } from "react-apollo";
-
-import { createHttpLink } from "apollo-link-http";
-import fetch from "node-fetch";
-import { InMemoryCache } from "apollo-cache-inmemory";
-
-import ApolloClient from "apollo-client";
-import config from "./temp/config";
 import GoogleAnalytics from "react-ga";
 
 const gaTrackingId = process.env.REACT_APP_GA ? process.env.REACT_APP_GA : "";
 GoogleAnalytics.initialize(gaTrackingId);
-
-const client = new ApolloClient({
-  link: createHttpLink({
-    uri: `${config.sitecoreApiHost}/sitecore/api/graph/items/master?sc_apikey=${config.sitecoreApiKey}`,
-    fetch: fetch as any,
-  }),
-  cache: new InMemoryCache(),
-});
 
 // This is the main entry point of the app invoked by the renderer (server or client rendering).
 const AppRoot = ({ Router }: { Router: any }) => {
@@ -45,9 +29,7 @@ const AppRoot = ({ Router }: { Router: any }) => {
       <RouteDataManager>
         <ComponentFactoryContextProvider componentFactory={componentFactory}>
           <ComponentsSitecoreContext.Provider value={sitecoreContextData}>
-            <ApolloProvider client={client}>
-              <Layout />
-            </ApolloProvider>
+            <Layout />
           </ComponentsSitecoreContext.Provider>
         </ComponentFactoryContextProvider>
       </RouteDataManager>
--- src/layout/Nav/index.js
+++ src/layout/Nav/index.js
@@ -2,8 +2,27 @@ import React, { useState } from "react";
 import RouterLink from "../../atoms/RouterLink";
 import { useScrollPosition } from "@n8tb1t/use-scroll-position";
 import Logo from "../../atoms/Logo";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCalendarCheck } from '@fortawesome/free-solid-svg-icons'
+
+const HamburgerIcon = () => (
+  <svg
+    className="fill-current h-6 w-6"
+    viewBox="0 0 20 20"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <title>Menu</title>
+    <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+  </svg>
+);
+
+const LockIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    className="fill-current h-6 w-6"
+  >
+    <path d="M11.85 17.56a1.5 1.5 0 0 1-1.06.44H10v.5c0 .83-.67 1.5-1.5 1.5H8v.5c0 .83-.67 1.5-1.5 1.5H4a2 2 0 0 1-2-2v-2.59A2 2 0 0 1 2.59 16l5.56-5.56A7.03 7.03 0 0 1 15 2a7 7 0 1 1-1.44 13.85l-1.7 1.71zm1.12-3.95l.58.18a5 5 0 1 0-3.34-3.34l.18.58L4 17.4V20h2v-.5c0-.83.67-1.5 1.5-1.5H8v-.5c0-.83.67-1.5 1.5-1.5h1.09l2.38-2.39zM18 9a1 1 0 0 1-2 0 1 1 0 0 0-1-1 1 1 0 0 1 0-2 3 3 0 0 1 3 3z" />
+  </svg>
+);
 
 const Nav = ({ route }) => {
   const [submenuVisible, setSubmenuVisible] = useState(false);
@@ -42,14 +61,7 @@ const Nav = ({ route }) => {
             onClick={() => setSubmenuVisible(!submenuVisible)}
             className="flex items-center p-1 text-orange-800 hover:text-gray-900"
           >
-            <svg
-              className="fill-current h-6 w-6"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <title>Menu</title>
-              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-            </svg>
+            <HamburgerIcon />
           </button>
         </div>
 
@@ -100,7 +112,12 @@ const Nav = ({ route }) => {
                 isScrolled ? "gradient text-white" : "bg-white text-gray-800"
               }`}
             >
-              <FontAwesomeIcon icon={faCalendarCheck} /> Register
+              <div className="flex items-center">
+                <div>
+                  <LockIcon />
+                </div>
+                <div className="ml-1">Register</div>
+              </div>
             </button>
           </RouterLink>
         </div>
