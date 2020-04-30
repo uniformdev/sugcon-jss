dab5e36423af0951f1b8990fb58ce20e5e2dd403
Alex Shyba <as@altola.co>
2020-04-29 17:20:11 -0700

    js optimization3: component lazy loading with react-loadable-visibility

---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+export { default as MapBlock } from "./MapBlock";
+export { default as VideoBlock } from "./VideoBlock";
+export { default as CtaBlock } from "./CtaBlock";
+export { default as WaveSplitter } from "./WaveSplitter";
+export { default as CtaSection } from "./CtaSection";
+export { default as TeaserSection } from "./TeaserSection";
+export { default as Hero } from "./Hero";
+export { default as PricingSection} from './PricingSection';
+export { default as Speakers} from './Speakers';
